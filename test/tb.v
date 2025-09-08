294401f4eaf1d6522c5f76cbeb3ba4497d04b243
dhanushgsalian <159764200+dhanushgsalian@users.noreply.github.com>
2025-09-08 14:50:04 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -40,7 +40,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_pipelined_risc_v_cpu user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
